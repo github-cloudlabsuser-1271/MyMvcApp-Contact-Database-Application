f9d91a649760b68bc2833a32c950b2fdd68d292c
github-cloudlabsuser-1271 <github_cloudlabsuser_1271@cloudlabsaiuser.com>
2024-12-18 07:46:08 +0000

    fixed1

--- Models/User.cs
+++ Models/User.cs
@@ -1,16 +1,13 @@
 
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System;
 
 namespace MyMvcApp.Models
 {
     public class User
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
 
     }
 }
